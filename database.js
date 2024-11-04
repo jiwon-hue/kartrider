4ca5c5004771a0245c995c28dfffd44654806b17
jiwon-hue <jjw77889900@gmail.com>
2024-11-05 04:50:03 +0900

    백엔드 연결 함수

---
+++ database.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+async function dataRes(barcode) {
+  try {
+    const response = await axios.get(`https://port-0-barcode-backend-m331910a6bdfd054.sel4.cloudtype.app/get-barcode/${barcode}`); //해당 링크에서 Get 요청 
+    return response.data; // 데이터를 반환하여 다른 파일에서 사용 가능하게 함
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    throw error; // 에러 발생 시 에러를 던져서 호출한 곳에서 처리하게 함
+  }
+}
+
+module.exports = dataRes;
